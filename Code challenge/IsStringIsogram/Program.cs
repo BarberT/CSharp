beb627c64d98fa481a57e20ce48ded8b00941ed4
Michael Thomas <mthomas31421@gmail.com>
2020-08-14 15:23:38 -0500

    First Push

---
+++ Code challenge/IsStringIsogram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace IsStringIsogram
+{
+    class Program
+    {
+
+        // Given a string S of lowercase alphabets, check if it is isogram or not.
+        // An Isogram is a string in which no letter occurs more than once.
+
+        public static void IsIsogram()
+        {
+            bool results = false;
+            int ogCount = 0;
+
+            //string originalString = "cat"; //"amanisaman";
+            string originalString = "amanisaman";
+
+            ogCount = originalString.Length;
+            string newString = new string(originalString.ToCharArray().Distinct().ToArray());
+
+            if(ogCount == newString.Length)
+            {
+                results = true;   
+            }
+
+            Console.WriteLine(string.Format("Is the string a Isogarm: {0}", results));
+
+        }
+        static void Main(string[] args)
+        {
+            IsIsogram();
+        }
+    }
+}
